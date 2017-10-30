f47c995a6bc2d6eaf826184837188be3463c916a
Patrick Dantas <patrickdsilva99@gmail.com>
2017-10-30 20:03:08 +0000

    Update nick.js

--- comandos/nick.js
+++ comandos/nick.js
@@ -2,7 +2,7 @@ function executarComandoNick(param, client){
     if(param){
 			var oldnick = client.nick;
 			client.nick = param;
-			client.irc_client.send('nick', client.nick);
+			client.irc_client.emit('nick', client.nick);
 			client.broadcast.emit('mudanca-de-nick', oldnick+' mudou seu nick para '+client.nick)
 	}
 }
