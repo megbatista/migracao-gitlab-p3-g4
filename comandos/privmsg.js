04c21168be8c2833fcc4bf8b2b63439feb271001
Patrick Dantas <patrickdsilva99@gmail.com>
2017-10-30 20:02:34 +0000

    adicionar privmsg.js

---
+++ comandos/privmsg.js
@@ -0,0 +1,29 @@
+function executarComandoPrivmsg(param, cl, clients, canais)
+{
+
+    if(param[1])
+    {
+        
+        var msg = [];
+        for(var i = 2; i < param.length ; i++)
+        {
+            msg[i-2] = param[i];
+        }
+
+        if(param[1][0] != '#' && clients.filter(client => client.nick === param[1]).length)
+        {
+
+            clients.forEach(function(client)
+            {
+
+                if(client.nick === param[1])
+                {
+                    cl.irc_client.emit('privmsg',client.nick.toString(), msg.toString());
+                    client.emit('message',cl.nick+': ' + msg.toString());
+                }
+            });
+        }
+    }   
+}
+
+module.exports = executarComandoPrivmsg;
