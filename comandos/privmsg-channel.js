c10e08b1e10ee067a4b80d9dc8185d3027a0cb90
scorp85 <vitorafaelgr@gmail.com>
2017-11-06 20:12:33 -0200

    em privmsgChnnl trocando ctcp por say

--- comandos/privmsg-channel.js
+++ comandos/privmsg-channel.js
@@ -1,6 +1,6 @@
 function PrivmsgChannel(msg, cl, clients, canais)
 {
     cl.broadcast.to(cl.canal).emit('message', cl.nick+': ' + msg);
-    cl.irc_client.ctcp(cl.canal, "PRIVMSG", msg);
+    cl.irc_client.say(cl.canal, msg);
 }
 module.exports = PrivmsgChannel;
