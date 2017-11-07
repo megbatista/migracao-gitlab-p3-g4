57a793d576e359ee3b54d0d0669e5f6f32f2998c
Vitor Rafael Rodrigues <vitorafaelgr@gmail.com>
2017-11-07 00:32:14 +0000

    Cliente IRC funcional

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var List = require('./comandos/list');
 var Ping = require('./comandos/ping');
 var Join = require('./comandos/join');
 var PrivmsgChannel = require('./comandos/privmsg-channel');
+//var Part = require('./comandos/part');
 var executarComandoInvite = require('./comandos/invite');
 var executarComandoWhois = require('./comandos/whois');
-
 
 io.use(socketio_cookieParser); //usa esse processador de cookies dentro do socketio
 //configuranco dos middlewares do express
@@ -27,14 +27,8 @@ app.use(bodyParser.urlencoded( { extended: true } ));
 app.use(cookieParser());
 app.use(express.static('public'));
 
-var proxies = {}; // mapa de proxys
-var clients = [];
 
-var nicks=[];
-var servidores=[];
-var canais=[];
 var proxy_id = 0;
-var irc_client;
 
 //O sistema inicia aqui, quando fazemos a requisicao para localhost:3000
 app.get('/', function (req, res) {
@@ -43,11 +37,6 @@ app.get('/', function (req, res) {
 	if ( req.cookies.servidor && req.cookies.nick  && req.cookies.canal ) 
 	{		
 		proxy_id++;
-
-		nicks[proxy_id] = req.cookies.nick;
-		servidores[proxy_id] = req.cookies.servidor;
-		canais[proxy_id] = req.cookies.canal;
-
 		//console.log('nick: '+req.cookies.nick+' servidor: '+req.cookies.servidor+' canal: '+req.cookies.canal);
 
 		res.cookie('id', proxy_id);
@@ -63,6 +52,7 @@ app.get('/', function (req, res) {
 //conecta cliente e servidor via websocket
 io.on('connection', function (socket) {
 	
+
 	proxies[proxy_id] = socket;
 	
 	var client = socket;
@@ -72,7 +62,7 @@ io.on('connection', function (socket) {
 	client.canal = canais[proxy_id];
 
 	//cria o cliente irc
-	irc_client = new irc.Client(client.servidor, client.nick);
+	var irc_client = new irc.Client(client.servidor, client.nick);
 
 	//o cliente irc vai ouvir respostas do servidor irc atraves dos eventos abaixo
 	//e a resposta sera repassada deste servidor para o index.html onde tem outros
@@ -83,6 +73,7 @@ io.on('connection', function (socket) {
 
 	irc_client.addListener('motd', function(motd){
 		socket.emit('motd', '<pre>'+motd+'</pre>');
+		Join(client, client.canal, proxy_id);
 	});
 
 	irc_client.addListener('error', function(message){
@@ -136,11 +127,20 @@ io.on('connection', function (socket) {
 		socket.emit('whois', info);
 	});
 
+	irc_client.addListener('message', function(nick, to, text, msg){
+		
+		console.log('mensagem: ' + msg);
+		var mensagem = '&lt' + nick + '&gt ' + text;
+		console.log('<' + nick + '>' + text);
+		socket.emit('message',mensagem);
+	});
+
 	client.irc_client = irc_client;
+
 
 	Join(client, client.canal, canais);
 
-	clients[proxy_id] = client;
+
 
 	//trata as mensagens vindas da interface web(index.html)
 	socket.on('message', function (msg) {
@@ -161,10 +161,10 @@ io.on('connection', function (socket) {
 				case '/MOTD': client.irc_client.send('motd');
 				break;
 
-				case '/PRIVMSG' : Privmsg(comando, client, clients, canais);
+				case '/PRIVMSG' : Privmsg(comando, client);
 				break;
 
-				case '/LIST' : List(client, canais);
+//				case '/LIST' : List(client);
 				break;
 
 				case '/QUIT': client.irc_client.emit('quit', client.nick, msg, client.canal.toString());
@@ -176,17 +176,22 @@ io.on('connection', function (socket) {
 				case '/PING' : Ping(client);
 				break;
 
-				case '/JOIN' : Join(client, comando[1], canais);							   
+
+				case '/JOIN' : Join(client, comando[1]);
 				break;
+
+//				case '/PART' : Part(client, comando);
+//				break;
 
 				case '/WHOIS': executarComandoWhois(comando[1],client);
 				break;
+
 			}
 
 		}
 		else
 		{
-			PrivmsgChannel(msg, client, clients, canais);
+			PrivmsgChannel(msg, client);
 		}
 	});
 });
--- comandos/privmsg.js
+++ comandos/privmsg.js
@@ -11,28 +11,11 @@ function Privmsg(param, cl, clients, canais)
             msg += param[i] + " ";
         }
 
-        // nick não pode começar com #.
-        // filtra no array que armazena os clientes conectados o param[1] e verifica se há correspondências
-        if(param[1][0] != '#' && clients.filter(client => client.nick === param[1]).length)
-        {      
-          
-            //executa essa função para cada cliente, e verifica se cada um tem o nick igual ao informado
-            clients.forEach(function(client)
-            {
-                if(client.nick === param[1])
-                {
-                    //cl é o cliente que realizou o comando privmsg e está enviado a mensagem.
-                    //esse evento é emitido para o listener da app.js, e de lá o socket.emit emitirá o evento privmsg para o index.html
-                    //o evento privmsg no index.html é o feedback para o cliente que realizou o comando privmsg saber que o comando foi executado com sucesso.
-                    cl.irc_client.emit('privmsg',client.nick);
+        cl.irc_client.say(param[1], msg);
 
-                    //agora emite o evento para o cliente que possui o nick igual ao param[1].
-                    //o evento envio-privmsg é captado pelo index.html, de forma que quem recebeu a mensagem saiba que ela foi enviada no privado
-                    client.irc_client.emit('envio-privmsg',cl.nick+': ' + msg);
+        client.emit('envio-privmsg',cl.nick+': ' + msg);
                     
-                }
-            });
-        }
+        
     } 
 
 }
